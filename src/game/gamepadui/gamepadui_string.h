9f975266e5d141f82fbd865e5c0ba414c8ce639e
Madi <madison@atlasdevs.net>
2023-02-13 08:31:04 -0800

    properly initialize UTF8 strings
    Thanks to Nbc66 and friends for catching this.

--- src/game/gamepadui/gamepadui_string.h
+++ src/game/gamepadui/gamepadui_string.h
@@ -100,7 +100,9 @@ public:
             return;
 
         wchar_t szUnicode[ 4096 ];
-        int nChars = V_UTF8ToUnicode( pszText, szUnicode, sizeof( szUnicode ) );
+		memset( szUnicode, 0, sizeof( wchar_t ) * 4096 );
+        
+		int nChars = V_UTF8ToUnicode( pszText, szUnicode, sizeof( szUnicode ) );
         if ( nChars > 1 )
             SetText( szUnicode, nChars - 1 );
     }
